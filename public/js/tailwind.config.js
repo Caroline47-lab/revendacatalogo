1e5164aeb9d25614b0fbb602c075b6f73afa8141
Caroline47-lab <cjotarasteirinhas@hotmail.com>
2025-07-21 20:08:52 -0300

    Create tailwind.config.js

---
+++ public/js/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/**/*.html', // Analisa todos os arquivos HTML na pasta public
+    './public/**/*.js',   // Analisa todos os arquivos JS na pasta public
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
