9ae5f41b0bb6548b5fb34b433076aa7c29aee9de
Caroline47-lab <cjotarasteirinhas@hotmail.com>
2025-07-22 14:29:32 -0300

    Update tailwind.config.js

--- public/js/tailwind.config.js
+++ public/js/tailwind.config.js
@@ -1,11 +1,1 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './public/**/*.html', // Analisa todos os arquivos HTML na pasta public
-    './public/**/*.js',   // Analisa todos os arquivos JS na pasta public
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+
